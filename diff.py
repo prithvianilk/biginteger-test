3971a8a32afcce293fae261c40be6844fa4d3cc1
Ubuntu <ubuntu@ip-172-31-1-254.ap-south-1.compute.internal>
2023-08-09 09:16:21 +0000

    diff + leqs

--- diff.py
+++ diff.py
@@ -1,7 +1,10 @@
 delta = 0
 with open('ok_1.txt') as f1:
     with open('ok_2.txt') as f2:
-        for (x, y) in zip(f1.readlines(), f2.readlines()):
+        l1, l2 = f1.readlines(), f2.readlines()
+        for (x, y) in zip(l1, l2):
             delta += int(x != y)
-print(delta)
+        total = set(l1 + l2)
+        print(len(total) == len(l1) + len(l2))
+        print(delta)
 
